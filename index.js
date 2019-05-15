301c27cef20151160b32196169a07e2ec80aff95
George Adams <georgeadams1995@gmail.com>
2019-05-15 13:33:30 +0100

    add array of versions

--- index.js
+++ index.js
@@ -8,33 +8,42 @@ glob("*.properties", function(er, files) {
   }
 });
 
-let options = {
-  method: 'GET',
-  url: 'https://api.adoptopenjdk.net/v2/latestAssets/releases/openjdk11',
-  qs: {
-    os: 'linux',
-    heap_size: 'normal'
-  },
-};
+const java_versions = [
+  "openjdk8",
+  "openjdk11",
+  "openjdk12"
+]
 
-request(options, function(error, response, body) {
-  if (error) throw new Error(error);
-  for (let release of JSON.parse(body)) {
-    let version = release.version_data.openjdk_version
-    let jvm = release.openjdk_impl
-    let architecture = release.architecture
-    let major_version = release.version
-    let link = release.release_link
+for (let java_version of java_versions) {
 
-    let properties = `VERSION=${version}\n` +
-      `JVM=${jvm}\n` +
-      `ARCHITECTURE=${architecture}\n` +
-      `MAJOR_VERSION=${major_version}\n` +
-      `LINK=${link}`
+  let options = {
+    method: 'GET',
+    url: `https://api.adoptopenjdk.net/v2/latestAssets/releases/${java_version}`,
+    qs: {
+      os: 'linux',
+      heap_size: 'normal'
+    },
+  };
 
-    fs.writeFile(`${architecture}-${jvm}-${version}.properties`, properties, (err) => {
-      // throws an error, you could also catch it here
-      if (err) throw err;
-    });
-  }
-});
+  request(options, function(error, response, body) {
+    if (error) throw new Error(error);
+    for (let release of JSON.parse(body)) {
+      let version = release.version_data.openjdk_version
+      let jvm = release.openjdk_impl
+      let architecture = release.architecture
+      let major_version = release.version
+      let link = release.release_link
+
+      let properties = `VERSION=${version}\n` +
+        `JVM=${jvm}\n` +
+        `ARCHITECTURE=${architecture}\n` +
+        `MAJOR_VERSION=${major_version}\n` +
+        `LINK=${link}`
+
+      fs.writeFile(`${architecture}-${jvm}-${version}.properties`, properties, (err) => {
+        // throws an error, you could also catch it here
+        if (err) throw err;
+      });
+    }
+  });
+}
