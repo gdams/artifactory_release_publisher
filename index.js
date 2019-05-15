b46c3950627ddfd30b2c68adcbbe682a066d40c7
George Adams <georgeadams1995@gmail.com>
2019-05-15 13:30:11 +0100

    initial config

---
+++ index.js
@@ -0,0 +1,40 @@
+const request = require("request");
+const glob = require("glob");
+const fs = require("fs");
+
+glob("*.properties", function(er, files) {
+  for (let file of files) {
+    fs.unlinkSync(file)
+  }
+});
+
+let options = {
+  method: 'GET',
+  url: 'https://api.adoptopenjdk.net/v2/latestAssets/releases/openjdk11',
+  qs: {
+    os: 'linux',
+    heap_size: 'normal'
+  },
+};
+
+request(options, function(error, response, body) {
+  if (error) throw new Error(error);
+  for (let release of JSON.parse(body)) {
+    let version = release.version_data.openjdk_version
+    let jvm = release.openjdk_impl
+    let architecture = release.architecture
+    let major_version = release.version
+    let link = release.release_link
+
+    let properties = `VERSION=${version}\n` +
+      `JVM=${jvm}\n` +
+      `ARCHITECTURE=${architecture}\n` +
+      `MAJOR_VERSION=${major_version}\n` +
+      `LINK=${link}`
+
+    fs.writeFile(`${architecture}-${jvm}-${version}.properties`, properties, (err) => {
+      // throws an error, you could also catch it here
+      if (err) throw err;
+    });
+  }
+});
